cb29b4a95eadd4a2d91aebfd4a93075898cb769d
Vasvi1599 <75876049+Vasvi1599@users.noreply.github.com>
2020-12-26 16:58:59 +0530

    Update positive numbers in range.py

--- positive numbers in range.py
+++ positive numbers in range.py
@@ -3,11 +3,11 @@
 list1= [ 12, -7, 5, 64, -14 ]
 print(list1)
 for number in list1:
-    if number >= 0:
+    if number >= 1:
         print( number , end = " ")
 print("")
 list2 = [ 12, 14, -95, 3 ]
 print(list2)
 for number in list2:
-    if number >= 0:
+    if number >= 1:
         print( number , end = " ")
